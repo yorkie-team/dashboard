54db99aa0fad10d6b08b7fdb479e210d340ce4c3
Yourim Cha <yourim.cha@navercorp.com>
2022-04-21 17:45:01 +0900

    Implement next-way pagination
    Co-authored-by: Youngteac Hong <youngteac.hong@navercorp.com>

--- src/features/documents/documentsSlice.ts
+++ src/features/documents/documentsSlice.ts
@@ -4,18 +4,22 @@ import { listDocuments, DocumentSummary } from '../../api';
 
 export interface DocumentsState {
   documents: Array<DocumentSummary>;
+  hasNext: boolean;
   status: 'idle' | 'loading' | 'failed';
 }
 
 const initialState: DocumentsState = {
   documents: [],
+  hasNext: false,
   status: 'idle',
 };
 
+const pageSize = 20;
+
 export const listDocumentsAsync = createAsyncThunk(
   'documents/listDocuments',
-  async (): Promise<Array<DocumentSummary>> => {
-    const documents = await listDocuments("", 20);
+  async (previousID?: string): Promise<Array<DocumentSummary>> => {
+    const documents = await listDocuments(previousID || '', pageSize + 1);
     return documents;
   }
 );
@@ -30,7 +34,8 @@ export const documentSlice = createSlice({
     });
     builder.addCase(listDocumentsAsync.fulfilled, (state, action) => {
       state.status = 'idle';
-      state.documents = action.payload;
+      state.documents = action.payload.slice(0, pageSize);
+      state.hasNext = action.payload.length === pageSize + 1;
     });
     builder.addCase(listDocumentsAsync.rejected, (state) => {
       state.status = 'failed';
@@ -38,7 +43,6 @@ export const documentSlice = createSlice({
   },
 });
 
-export const selectDocuments = (state: RootState) => state.documents.documents;
-export const selectStatus = (state: RootState) => state.documents.status;
+export const selectDocuments = (state: RootState) => state.documents;
 
 export default documentSlice.reducer;
