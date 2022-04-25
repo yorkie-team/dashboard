09530ea9784970e95002193729e13fb165608256
Yourim Cha <yourim.cha@navercorp.com>
2022-04-25 16:35:29 +0900

    Style disabled button

--- src/features/documents/DocumentList.tsx
+++ src/features/documents/DocumentList.tsx
@@ -31,15 +31,15 @@ export function DocumentList() {
         <ul>
           {documents.map((document) => (
             <li key={document.id}>
-              {document.key.collection}${document.key.document}{' '}
-              {document.snapshot}
+              {document.key.collection}${document.key.document} {document.snapshot}
             </li>
           ))}
         </ul>
       )}
       <div className='flex items-center space-x-2'>
         <button
-          className='relative inline-flex items-center justify-center text-center py-1.5 px-3 bg-gray-100 hover:bg-gray-200 border border-gray-300 rounded-lg focus:outline-none focus-visible:ring-4 focus-visible:ring-gray-200 font-medium text-gray-900 text-sm'
+          className='relative inline-flex items-center justify-center text-center py-1.5 px-3 bg-gray-100 hover:bg-gray-200 disabled:!bg-gray-300 border border-gray-300 rounded-lg focus:outline-no
+          ne focus-visible:ring-4 focus-visible:ring-gray-200 font-medium text-gray-900 text-sm disabled:text-gray-400 disabled:cursor-not-allowed'
           type='button'
           onClick={handlePrevBtnClicked}
         >
@@ -57,17 +57,14 @@ export function DocumentList() {
                 clipRule='evenodd'
                 d='M6.46961 13.5303L1.93928 8.99994L6.46961 4.46961L7.53027 5.53027L3.86 8.99994L7.53027 12.4696L6.46961 13.5303Z'
               ></path>
-              <path
-                fillRule='evenodd'
-                clipRule='evenodd'
-                d='M13 9.75L3.5 9.65L3.5 8.3501L13 8.25V9.75Z'
-              ></path>
+              <path fillRule='evenodd' clipRule='evenodd' d='M13 9.75L3.5 9.65L3.5 8.3501L13 8.25V9.75Z'></path>
             </svg>
             Previous
           </span>
         </button>
         <button
-          className='relative inline-flex items-center justify-center text-center py-1.5 px-3 bg-gray-100 hover:bg-gray-200 border border-gray-300 rounded-lg focus:outline-none focus-visible:ring-4 focus-visible:ring-gray-200 font-medium text-gray-900 text-sm'
+          className='relative inline-flex items-center justify-center text-center py-1.5 px-3 bg-gray-100 hover:bg-gray-200 disabled:!bg-gray-300 border border-gray-300 rounded-lg focus:outline-no
+          ne focus-visible:ring-4 focus-visible:ring-gray-200 font-medium text-gray-900 text-sm disabled:text-gray-400 disabled:cursor-not-allowed'
           type='button'
           onClick={handleNextBtnClicked}
           disabled={!hasNext}
@@ -87,11 +84,7 @@ export function DocumentList() {
                 clipRule='evenodd'
                 d='M9.53039 4.46973L14.0607 9.00006L9.53039 13.5304L8.46973 12.4697L12.14 9.00006L8.46973 5.53039L9.53039 4.46973Z'
               ></path>
-              <path
-                fillRule='evenodd'
-                clipRule='evenodd'
-                d='M3 8.25L12.5 8.35V9.65L3 9.75V8.25Z'
-              ></path>
+              <path fillRule='evenodd' clipRule='evenodd' d='M3 8.25L12.5 8.35V9.65L3 9.75V8.25Z'></path>
             </svg>
           </span>
         </button>
