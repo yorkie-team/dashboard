27e69f536dccd68d0a170a1fc319840f06ecbf4c
Yourim Cha <yourim.cha@navercorp.com>
2022-04-27 13:57:48 +0900

    Add document pagination test code
    - To check the disabled state of previous and next buttons

--- src/features/documents/documentsSlice.ts
+++ src/features/documents/documentsSlice.ts
@@ -30,14 +30,30 @@ export const listDocumentsAsync = createAsyncThunk(
   }> => {
     const { isForward, previousID = '' } = params;
     const documents = await listDocuments(previousID, pageSize + 1, isForward);
-    const isFull = documents.length === pageSize + 1;
-    return {
-      documents: !isFull ? documents : isForward ? documents.slice(1, pageSize + 1) : documents.slice(0, pageSize),
-      hasPrevious: !!previousID && (isFull || !isForward),
-      hasNext: isFull || (!isFull && isForward),
-    };
+
+    return getPaginationData({ documents, isForward, previousID, pageSize });
   }
 );
+
+export const getPaginationData = (params: {
+  documents: Array<DocumentSummary>;
+  isForward: boolean;
+  previousID: string;
+  pageSize: number;
+}): {
+  documents: Array<DocumentSummary>;
+  hasNext: boolean;
+  hasPrevious: boolean;
+} => {
+  const { isForward, previousID, documents, pageSize } = params;
+  const isFull = documents.length === pageSize + 1;
+
+  return {
+    documents: !isFull ? documents : isForward ? documents.slice(1, pageSize + 1) : documents.slice(0, pageSize),
+    hasPrevious: !!previousID && (isFull || !isForward),
+    hasNext: isFull || (!isFull && isForward),
+  };
+};
 
 export const documentSlice = createSlice({
   name: 'documents',
