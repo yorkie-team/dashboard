3948fb505ab85db07337a7a1463d7090f257392f
Yourim Cha <81357083+chacha912@users.noreply.github.com>
2022-05-13 19:21:16 +0900

    Display document JSON  (#15)
    * Display document JSON using react-json-view
    * TODO: escape ", \n, \r, \t in server-side

--- src/features/documents/DocumentDetail.tsx
+++ src/features/documents/DocumentDetail.tsx
@@ -1,12 +1,30 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import ReactJson from 'react-json-view';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
 import { selectDocumentDetail, getDocumentAsync } from './documentsSlice';
 
+function getDocumentJSON(snapshot: string | undefined): object {
+  if (!snapshot) {
+    return {};
+  }
+  // TODO(chacha): escape ", \n, \r, \t in server-side
+  return JSON.parse(filterEscape(snapshot));
+}
+
+function filterEscape(str: string): string {
+  return str //
+    .replace(/\n/g, '\\n')
+    .replace(/\r/g, '\\r')
+    .replace(/\t/g, '\\t');
+}
+
+// DocumentDetail represents the snapshot of document.
 export function DocumentDatail() {
   const { document, status } = useAppSelector(selectDocumentDetail);
   const dispatch = useAppDispatch();
   const { documentID } = useParams();
+  const documentJson = getDocumentJSON(document?.snapshot);
 
   useEffect(() => {
     dispatch(getDocumentAsync(documentID!));
@@ -19,7 +37,13 @@ export function DocumentDatail() {
       {status === 'idle' && (
         <>
           <h2 className="text-lg font-semibold">{document?.key}</h2>
-          <div className="py-6">{document?.snapshot}</div>
+          <div className="py-6">
+            <ReactJson
+              src={documentJson}
+              displayObjectSize={false}
+              displayDataTypes={false}
+            />
+          </div>
         </>
       )}
     </div>
