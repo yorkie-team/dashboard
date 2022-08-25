6f4ce9cec7d652154d5d234ee9b9835b40de7a36
Youngteac Hong <susukang98@gmail.com>
2022-08-25 22:28:37 +0900

    Extract AccountButton from Header to keep "Feature Folders"

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -14,37 +14,12 @@
  * limitations under the License.
  */
 
-import React, { useRef, useEffect, useState, useCallback } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { useAppSelector, useAppDispatch } from 'app/hooks';
-import { logoutUser } from 'features/users/usersSlice';
+import { AccountButton } from 'features/users/AccountButton';
 
 export function Header() {
-  const { token, username } = useAppSelector((state) => state.users);
-  const userDropdownRef = useRef<HTMLDivElement | null>(null);
-  const [isUserDropdownOpen, setIsUserDropdownOpen] = useState(false);
-
-  const dispatch = useAppDispatch();
-  const logout = useCallback(() => {
-    dispatch(logoutUser());
-  }, [dispatch]);
-
-  const handleClickOutside = useCallback(
-    (e) => {
-      if (!isUserDropdownOpen) return;
-      if (!userDropdownRef.current?.contains(e.target)) setIsUserDropdownOpen(false);
-    },
-    [isUserDropdownOpen],
-  );
-
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutside);
-    return () => {
-      document.removeEventListener('click', handleClickOutside);
-    };
-  }, [handleClickOutside]);
-
   return (
     <nav className="bg-white border-gray-200 py-1 px-2 sm:px-4 rounded">
       <div className="flex flex-wrap justify-between items-center mx-auto">
@@ -64,64 +39,7 @@ export function Header() {
             />
           </svg>
         </Link>
-        {token && (
-          <>
-            <button
-              type="button"
-              className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-            >
-              <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
-            <div className="hidden relative w-full items-center sm:flex sm:w-auto">
-              <ul className="inline-flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 sm:flex-row sm:space-x-8 sm:mt-0 sm:text-sm sm:font-medium sm:border-0 sm:bg-white">
-                <li>
-                  <a
-                    href="https://yorkie.dev/docs"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 sm:hover:bg-transparent sm:border-0 sm:hover:text-orange-500 sm:p-0"
-                  >
-                    Docs
-                  </a>
-                </li>
-              </ul>
-              <button
-                type="button"
-                className="inline-flex items-center justify-center ml-3 w-6 h-6 rounded-full bg-orange-300 uppercase"
-                onClick={() => setIsUserDropdownOpen(true)}
-              >
-                {username.slice(0, 1)}
-              </button>
-              {isUserDropdownOpen && (
-                <div
-                  ref={userDropdownRef}
-                  className="absolute z-10 top-12 right-0 bg-white rounded drop-shadow-lg py-1 min-w-[12rem]"
-                >
-                  <div className="py-3 px-4 text-sm text-gray-900">
-                    <div className="font-medium">{username}</div>
-                  </div>
-                  <ul className="border-t border-solid border-gray-200 py-1 text-sm text-gray-700">
-                    <li>
-                      <button
-                        type="button"
-                        onClick={logout}
-                        className="block w-full py-2 px-4 hover:bg-gray-100 text-left"
-                      >
-                        Sign out
-                      </button>
-                    </li>
-                  </ul>
-                </div>
-              )}
-            </div>
-          </>
-        )}
+        <AccountButton />
       </div>
     </nav>
   );
