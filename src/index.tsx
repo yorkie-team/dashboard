bda771d47d9ac57d4552eace266a14de2b3c493c
Yourim Cha <yourim.cha@navercorp.com>
2022-09-30 14:05:35 +0900

    Remove Tailwind CSS

--- src/index.tsx
+++ src/index.tsx
@@ -16,7 +16,6 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
