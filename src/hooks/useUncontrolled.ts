29d1573f2cf215784ea37780dc1adad14fb59057
Youngteac Hong <susukang98@gmail.com>
2022-11-21 15:02:38 +0900

    Apply the latest UI
    - community page
    - 404 page

--- src/hooks/useUncontrolled.ts
+++ src/hooks/useUncontrolled.ts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 The Yorkie Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { useState } from 'react';
 
 interface UseUncontrolledInput<T> {
@@ -18,7 +34,7 @@ export function useUncontrolled<T>({
   value,
   defaultValue,
   finalValue,
-  onChange = () => {},
+  onChange = () => { },
 }: UseUncontrolledInput<T>): [T, (value: T) => void, boolean] {
   const [uncontrolledValue, setUncontrolledValue] = useState(defaultValue !== undefined ? defaultValue : finalValue);
 
