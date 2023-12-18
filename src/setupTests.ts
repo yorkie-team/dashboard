9d4a22938ac83223e5755316ce10fd048b9061d0
Yourim Cha <yourim.cha@navercorp.com>
2023-12-18 23:47:20 +0900

    Fix errors occurring during test execution

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// NOTE(chacha912): This patches the global objects TextEncoder, TextDecoder
+// which are missing in the JSDOM environment.
+// See https://github.com/jsdom/jsdom/issues/2524 for more details.
+import { TextEncoder, TextDecoder } from 'util';
+
+Object.assign(global, { TextDecoder, TextEncoder });
