1ac50c6a135c8126af27c9da2d91367b22442f85
Vi Tran ( Alice) <vi.tran@navercorp.com>
2024-06-20 15:52:24 +0700

    update ui libabry and version

--- src/index.tsx
+++ src/index.tsx
@@ -19,6 +19,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import 'yorkie-ui/style';
 import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById('root');
