6da62d74b1ce1a5962f5699fdd8db6e1ba99c59d
Yourim Cha <yourim.cha@navercorp.com>
2024-06-25 02:33:58 +0900

    Refactor imports in SignupForm component

--- src/features/users/SignupForm.tsx
+++ src/features/users/SignupForm.tsx
@@ -15,12 +15,11 @@
  */
 
 import React, { useEffect, useCallback } from 'react';
-import { useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { SignupFields, selectUsers, signupUser, resetSignupState } from './usersSlice';
 import { Button, InputTextBox } from 'components';
-import { useAppDispatch } from 'app/hooks';
+import { useAppDispatch, useAppSelector } from 'app/hooks';
 
 export function SignupForm() {
   const dispatch = useAppDispatch();
@@ -35,7 +34,7 @@ export function SignupForm() {
   } = useForm<SignupFields>();
   const {
     signup: { isSuccess, status, error },
-  } = useSelector(selectUsers);
+  } = useAppSelector(selectUsers);
 
   const onSubmit = useCallback(
     (data: SignupFields) => {
