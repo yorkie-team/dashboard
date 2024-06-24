69f0f54a5d6678a79bc1426b39f5ea588b639b88
Yourim Cha <yourim.cha@navercorp.com>
2024-06-25 03:26:21 +0900

    Remove unused code related to CRA

--- src/main.tsx
+++ src/main.tsx
@@ -15,12 +15,10 @@
  */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -29,8 +27,3 @@ createRoot(document.getElementById('root')!).render(
     </Provider>
   </React.StrictMode>,
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
