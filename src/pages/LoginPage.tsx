cf28fc1d61e296edc21a4c217f07a792ae2fe6cf
Youngteac Hong <susukang98@gmail.com>
2025-03-14 18:53:10 +0900

    Add GitHub and Username sign-in options to LoginPage

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -14,17 +14,37 @@
  * limitations under the License.
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { LoginForm } from 'features/users';
 import { PageTemplate } from './PageTemplate';
 import { Icon, Button } from 'components';
 
 export function LoginPage() {
+  const [withUsername, setWithUsername] = useState(false);
+
   return (
     <PageTemplate className="login_page">
       <Icon type="logo3d" className="icon_logo" fill />
       <h2 className="title">Sign in to Yorkie</h2>
-      <LoginForm />
+      {!withUsername && (
+        <div style={{ width: 336 }}>
+          <div style={{ marginTop: '5rem' }}>
+            <Button.Box fullWidth={true}>
+              <Button as="link" href={`${import.meta.env.VITE_API_ADDR}/auth/github/login`} outline={true}>
+                Sign in with GitHub
+              </Button>
+            </Button.Box>
+          </div>
+          <div style={{ marginTop: '2rem' }}>
+            <Button.Box fullWidth={true}>
+              <Button onClick={() => setWithUsername(true)} outline={true}>
+                Sign in with Username
+              </Button>
+            </Button.Box>
+          </div>
+        </div>
+      )}
+      {withUsername && <LoginForm />}
       <div className="box_bottom">
         <Button.Box fullWidth={true}>
           <Button as="link" href="/signup" outline={true}>
