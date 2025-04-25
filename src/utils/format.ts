eb040e594f88f6c6f611c4b9a0a134e90cc2bfc8
raararaara <raararaara@gmail.com>
2025-04-25 18:37:12 +0900

    Apply coderabbit's suggestion

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -7,7 +7,7 @@ export const formatNumber = (num: number | undefined) => {
 };
 
 export const humanFileSize = (size: number | undefined) => {
-  if (size == undefined) return 0;
+  if (size == undefined) return '0 B';
   let i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
   return Number((size / Math.pow(1024, i)).toFixed(2)) + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
 }
